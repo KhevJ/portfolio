061882a00662e315163a3bd50d4c34d2c8a2a68c
KhevJ <100340684+KhevJ@users.noreply.github.com>
2023-11-27 03:33:55 -0700

    added some projects

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect, useCallback } from 'react';
 import BodyContainer from "./BodyContainer";
 import {Image} from 'react-native';
+import ChessVideo from '../assets/chess-game-video.mp4'
 import styles from "../modules/Body.module.css"
 
 
@@ -35,7 +36,7 @@ export default function Body(){
     return (
         <main className="mx-5 mt-16 md:relative md:left-[48vw] md:top-0 md:mx-0 md:mt-0 md:w-[39.5vw] md:pl-[4vw] md:pt-[11vh]">
             <section id="projects-section">
-                <h2 className="section-title mb-3 font-raleway text-3xl font-extrabold text-black lg:mb-4 lg:text-4xl xl:mb-5 2xl:text-5xl">Projects</h2>
+                <h2 className="section-title mb-3 font-rajdhani text-3xl font-extrabold text-black lg:mb-4 lg:text-4xl xl:mb-5 2xl:text-5xl">Projects</h2>
                 <BodyContainer
                 expanded={sectionExpandedP1}
                 expandFunction={setSectionExpandedP1}
@@ -54,10 +55,11 @@ export default function Body(){
                     <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">Material UI</span>
                     <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">ExpressJS</span>
                     <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">NodeJS</span>
-                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">TCP connection</span>
+                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">socket.io</span>
+                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">SeleniumIDE</span>
                 </div>
                 <p className="section-paragraph text-xs font-medium text-black lg:text-xs lg:leading-normal xl:text-sm 2xl:text-base mb-3 xl:mb-3.5 2xl:mb-4">
-                    This is a chess game where you can challenge your friends to game of chess with all chess functionalities. The connection between the 2 players were doen using a TCP connection with expressJS and NodeJS. The front-end was made using ReactJS and Material UI library.
+                    This is a chess game where you can challenge your friends to game of chess. The connection between the 2 players were done using a TCP connection using socket.io with expressJS and NodeJS . The front-end was made using ReactJS and Material UI library. The testing was done using the SeleniumIDE.
                 </p>
                 <div
                     className={`${
@@ -67,7 +69,7 @@ export default function Body(){
                     <div className="overflow-hidden ">
                         <div
                             onClick={(event) => {
-                            window.open("https://github.com/techstartucalgary/EasyMeal");
+                            window.open("https://github.com/KhevJ/chess-app");
                             event.stopPropagation();
                             }}
                             className="github-link-button flex w-fit flex-row items-center gap-1.5 rounded bg-black py-1.5 pl-2 outline-none hover:bg-red-400  lg:gap-2 lg:py-2 lg:pl-3"
@@ -95,7 +97,72 @@ export default function Body(){
                     <div className='overflow-hidden '>
                         <div
                             onClick={(event) => {
-                            window.open("https://github.com/techstartucalgary/EasyMeal");
+                            window.open("https://chess-game-8e69b6359bbf.herokuapp.com");
+                            event.stopPropagation();
+                            }}
+                            className="github-link-button flex w-fit flex-row items-center gap-1.5 rounded bg-black py-1.5 pl-2 outline-none hover:bg-red-400  lg:gap-2 lg:py-2 lg:pl-3"
+                        >
+                            <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="1em"
+                            height="1em"
+                            viewBox="0 0 256 256"
+                            className='h-3 w-3 fill-white lg:h-3.5 lg:w-3.5 xl:h-4 xl:w-4'
+                            >
+                            <path d="M136.37 187.53a12 12 0 010 17l-5.94 5.94a60 60 0 01-84.88-84.88l24.12-24.11A60 60 0 01152 99a12 12 0 11-16 18 36 36 0 00-49.37 1.47l-24.1 24.08a36 36 0 0050.92 50.92l5.94-5.94a12 12 0 0116.98 0zm74.08-142a60.09 60.09 0 00-84.88 0l-5.94 5.94a12 12 0 0017 17l5.94-5.94a36 36 0 0150.92 50.92l-24.11 24.12A36 36 0 01120 139a12 12 0 10-16 18 60 60 0 0082.3-2.43l24.12-24.11a60.09 60.09 0 00.03-84.91z" />
+                            </svg>
+                            <span className='pr-3 text-2xs font-semibold text-white lg:pr-4 lg:text-xs xl:text-sm'>App Link</span>
+                        </div>
+                    </div>
+                    </div>
+                    <div
+                    className={`${
+                        sectionExpandedP1 && styles.section_showcase_expanded 
+                        } ${styles.section_showcase_container }`}
+                     >
+                    <div className='overflow-hidden '>
+                    <h4 className="section-header-hidden mb-2 mt-4 text-xs font-semibold text-black  lg:text-sm xl:mt-5 xl:text-base 2xl:mt-6 2xl:text-lg">Video Demo</h4>
+                    
+                    <video src={ChessVideo} controls="controls"></video>
+                    </div>
+                    
+                    
+                    </div>
+                </div>
+              </BodyContainer>
+                
+              {/* Project 2 */}
+              <BodyContainer
+                expanded={sectionExpandedP2}
+                expandFunction={setSectionExpandedP2}
+                
+                >
+                <h3 className="section-header text-xs font-bold text-black  lg:text-sm xl:text-base 2xl:text-lg">Samurai Fight Game</h3>
+                <div className="section-subheader-container mt-1 flex flex-row items-center gap-2 text-xs font-semibold text-black  lg:text-2xs xl:mt-1.5 xl:gap-2.5 xl:text-xs 2xl:mt-2 2xl:gap-3 2xl:text-base">
+                    <span className="shrink-0">Frontend Developer</span>
+                    <div className="dot h-[2px] w-[2px] rounded-full bg-black xl:h-[3px] xl:w-[3px] 2xl:h-1 2xl:w-1"></div>
+                    <span className="section-date truncate pr-1 italic">
+                    February 2023 - April 2023
+                    </span>
+                </div>
+                <div className="section-tag-container my-2 flex flex-row flex-wrap items-center gap-2 xl:my-2.5 xl:gap-2.5 2xl:my-3 2xl:gap-3">
+                    <span className="section-tag border-black  rounded-full  bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">HTML/CSS</span>
+                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">Javascript</span>
+                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">Jest</span>
+        
+                </div>
+                <p className="section-paragraph text-xs font-medium text-black lg:text-xs lg:leading-normal xl:text-sm 2xl:text-base mb-3 xl:mb-3.5 2xl:mb-4">
+                    This is a chess game where you can challenge your friends to game of chess. The connection between the 2 players were done using a TCP connection using socket.io with expressJS and NodeJS . The front-end was made using ReactJS and Material UI library. The testing was done using the SeleniumIDE.
+                </p>
+                <div
+                    className={`${
+                        sectionExpandedP2 && styles.section_showcase_expanded 
+                        } ${styles.section_showcase_container }`}
+                >
+                    <div className="overflow-hidden ">
+                        <div
+                            onClick={(event) => {
+                            window.open("https://github.com/KhevJ/SamuraiFight");
                             event.stopPropagation();
                             }}
                             className="github-link-button flex w-fit flex-row items-center gap-1.5 rounded bg-black py-1.5 pl-2 outline-none hover:bg-red-400  lg:gap-2 lg:py-2 lg:pl-3"
@@ -111,14 +178,108 @@ export default function Body(){
                             </svg>
                             <span className='pr-3 text-2xs font-semibold text-white lg:pr-4 lg:text-xs xl:text-sm'>Link to Github repository</span>
                         </div>
+
+                    
+                    
+                    </div>
+                    <div
+                    className={`${
+                        sectionExpandedP2 && styles.section_showcase_expanded 
+                        } ${styles.section_showcase_container }`}
+                >
+                    <div className='overflow-hidden '>
+                        <div
+                            onClick={(event) => {
+                            window.open("https://samurai-fight-khevj.vercel.app");
+                            event.stopPropagation();
+                            }}
+                            className="github-link-button flex w-fit flex-row items-center gap-1.5 rounded bg-black py-1.5 pl-2 outline-none hover:bg-red-400  lg:gap-2 lg:py-2 lg:pl-3"
+                        >
+                            <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="1em"
+                            height="1em"
+                            viewBox="0 0 256 256"
+                            className='h-3 w-3 fill-white lg:h-3.5 lg:w-3.5 xl:h-4 xl:w-4'
+                            >
+                            <path d="M136.37 187.53a12 12 0 010 17l-5.94 5.94a60 60 0 01-84.88-84.88l24.12-24.11A60 60 0 01152 99a12 12 0 11-16 18 36 36 0 00-49.37 1.47l-24.1 24.08a36 36 0 0050.92 50.92l5.94-5.94a12 12 0 0116.98 0zm74.08-142a60.09 60.09 0 00-84.88 0l-5.94 5.94a12 12 0 0017 17l5.94-5.94a36 36 0 0150.92 50.92l-24.11 24.12A36 36 0 01120 139a12 12 0 10-16 18 60 60 0 0082.3-2.43l24.12-24.11a60.09 60.09 0 00.03-84.91z" />
+                            </svg>
+                            <span className='pr-3 text-2xs font-semibold text-white lg:pr-4 lg:text-xs xl:text-sm'>App Link</span>
+                        </div>
                     </div>
                     </div>
+                   
                 </div>
                 </BodyContainer>
+
+
+            {/* Project 3 */}
+            <BodyContainer
+                expanded={sectionExpandedP3}
+                expandFunction={setSectionExpandedP3}
                 
-              
+                >
+                <h3 className="section-header text-xs font-bold text-black  lg:text-sm xl:text-base 2xl:text-lg">Chess Multiplayer</h3>
+                <div className="section-subheader-container mt-1 flex flex-row items-center gap-2 text-xs font-semibold text-black  lg:text-2xs xl:mt-1.5 xl:gap-2.5 xl:text-xs 2xl:mt-2 2xl:gap-3 2xl:text-base">
+                    <span className="shrink-0">Java Developer</span>
+                    <div className="dot h-[2px] w-[2px] rounded-full bg-black xl:h-[3px] xl:w-[3px] 2xl:h-1 2xl:w-1"></div>
+                    <span className="section-date truncate pr-1 italic">
+                    Nov 2022 - Jan 2023
+                    </span>
+                </div>
+                <div className="section-tag-container my-2 flex flex-row flex-wrap items-center gap-2 xl:my-2.5 xl:gap-2.5 2xl:my-3 2xl:gap-3">
+                    <span className="section-tag border-black  rounded-full  bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">Java</span>
+                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">Spring</span>
+                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">Design Patterns</span>
+                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">OOP</span>
+                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">SQL</span>
+                    <span className="section-tag rounded-full bg-black px-2 py-0.5 text-center text-xs font-normal text-pink-300 lg:px-2.5 lg:text-2xs xl:py-1 xl:text-xs 2xl:px-3 2xl:text-sm">MySQL</span>
+
+                </div>
+                <p className="section-paragraph text-xs font-medium text-black lg:text-xs lg:leading-normal xl:text-sm 2xl:text-base mb-3 xl:mb-3.5 2xl:mb-4">
+                    This is a movie theatre app made with Java using the spring package for the GUI and SQL for the database. This project was made using OOP principles such as aggregation, composition and inheritance. This project uses two design patterns: singleton pattern for the database and decorators pattern for the GUI. The app allows 
+                    people to either become a registered user or a regular user where a registered user will get a discount purchasing a movie ticket. It also allows user to select their movie and seat. All the information is stored in a database.</p>
+                <div
+                    className={`${
+                        sectionExpandedP3 && styles.section_showcase_expanded 
+                        } ${styles.section_showcase_container }`}
+                >
+                    <div className="overflow-hidden ">
+                        <div
+                            onClick={(event) => {
+                            window.open("https://github.com/KhevJ/MovieTheatreProject");
+                            event.stopPropagation();
+                            }}
+                            className="github-link-button flex w-fit flex-row items-center gap-1.5 rounded bg-black py-1.5 pl-2 outline-none hover:bg-red-400  lg:gap-2 lg:py-2 lg:pl-3"
+                        >
+                            <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="1em"
+                            height="1em"
+                            viewBox="0 0 256 256"
+                            className='h-3 w-3 fill-white lg:h-3.5 lg:w-3.5 xl:h-4 xl:w-4'
+                            >
+                            <path d="M136.37 187.53a12 12 0 010 17l-5.94 5.94a60 60 0 01-84.88-84.88l24.12-24.11A60 60 0 01152 99a12 12 0 11-16 18 36 36 0 00-49.37 1.47l-24.1 24.08a36 36 0 0050.92 50.92l5.94-5.94a12 12 0 0116.98 0zm74.08-142a60.09 60.09 0 00-84.88 0l-5.94 5.94a12 12 0 0017 17l5.94-5.94a36 36 0 0150.92 50.92l-24.11 24.12A36 36 0 01120 139a12 12 0 10-16 18 60 60 0 0082.3-2.43l24.12-24.11a60.09 60.09 0 00.03-84.91z" />
+                            </svg>
+                            <span className='pr-3 text-2xs font-semibold text-white lg:pr-4 lg:text-xs xl:text-sm'>Link to Github repository</span>
+                        </div>
+
+                    
+                    
+                    </div>
+                    <div
+                    className={`${
+                        sectionExpandedP3 && styles.section_showcase_expanded 
+                        } ${styles.section_showcase_container }`}
+                >
+                    
+                    </div>
+            
+                </div>
+              </BodyContainer>
+            
         {/* Projects subsection (older projects) */}
-            <button
+            {/* <button
             onClick={toggleUnhideOtherProjects}
             className="mx-auto mb-5 flex flex-row items-center justify-center gap-2 outline-none lg:mb-6 lg:gap-2.5 xl:mb-7 xl:gap-3 2xl:mb-8 2xl:gap-4"
             >
@@ -157,7 +318,7 @@ export default function Body(){
             >
                 <path d="M6.906 6.594l-.718.687-3.907 3.907-.687.718L16 26.312l14.406-14.406-.687-.719-3.907-3.906-.718-.687L16 15.687zm-.031 2.843l8.406 8.376.719.687.719-.688 8.406-8.375 2.438 2.438L16 23.469 4.437 11.875z" />
             </svg>
-            </button>
+            </button> */}
             </section>
 
             <section id="education-section">
